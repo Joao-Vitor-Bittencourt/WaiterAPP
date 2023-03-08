9b43119a477d884cd321143b3ba9784dc501f40c
João Bittencourt <joaobittencourtd3v@gmail.com>
2023-03-08 00:32:25 -0300

    creating models of our database

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,15 @@
 import express from 'express';
+import mongoose from 'mongoose';
+
+mongoose.set('strictQuery', true);
+
+mongoose.connect('mongodb://localhost:27017')
+  .then(() => {
+    const app = express();
+    const port = 3001;
+
+    app.listen(port, () => {
+      console.log(`🚀 Server running on http://localhost:${port}`);
+    });
+  })
+  .catch(() => console.log('erro na conexão com o mongo'));
